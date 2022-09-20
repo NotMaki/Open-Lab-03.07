81bd10576919301f315f39bdd69737f61f7fe85e
Marko <jantulamarko@outlook.com>
2022-09-20 18:31:06 +0200

    Update Divider.cs

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._07
 {
@@ -6,7 +6,10 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if(people * each > total)
+                return false;
+            else
+                return true;
         }
     }
 }
